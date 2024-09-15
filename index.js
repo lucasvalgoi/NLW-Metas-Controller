014ff6e2e6d07501fc3622933acb3ea165fbec2a
Lucas Luis Valgoi <lucasluisvalgoi@gmail.com>
2024-09-15 20:53:26 -0300

    adicionando início menu aplicação

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const start = () => {
+const { select } = require('@inquirer/prompts')
+
+// O uso da Async function é de grande importânica para informar ao computador que ao invés de sair rodando todo o laço While enquanto a condição for True, será necessário esperar o usuário selecionar alguma das opções 
+const start = async () => {
     
     while (true) {
-        let opcao = "cadastrar"
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar",
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar",
+                },
+                {
+                    name: "Sair",
+                    value: "sair",
+                }
+            ]
+        })
+
+
         switch (opcao) {
             case "cadastrar":
                 console.log("Bora Cadastrar")
@@ -10,6 +32,7 @@ const start = () => {
                 console.log("Bora Listar")
                 break;
             case "sair":
+                console.log("Até mais!")
                 return
         }
     }
--- notas.md
+++ notas.md
@@ -1,3 +1,3 @@
 ## Começando o projeto:
-- Para começar o projeto, é necessário instalar via NPM o pacote `inquirer`
+- Para começar o projeto, é necessário instalar via NPM o pacote da Biblioteca `inquirer` para criar prompts interativos
      -> Use o comando `npm install inquirer` para instalar as dependências
